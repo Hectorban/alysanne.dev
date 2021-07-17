9b1212573ccc2d4fed917db5a68749d07e6ae4fa
Hectorban <hectorban1234@gmail.com>
2021-07-17 05:44:34 -0500

    Update theme.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -30,93 +30,93 @@ const theme = {
     heading: 700,
     display: 900
   },
-  "lineHeights": {
-    "body": 1.5,
-    "heading": 1.25
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25
   },
-  "textStyles": {
-    "heading": {
-      "fontFamily": "heading",
-      "fontWeight": "heading",
-      "lineHeight": "heading"
+  textStyles: {
+    heading: {
+      fontFamily: "heading",
+      fontWeight: "heading",
+      lineHeight: "heading"
     },
   },
-  "styles": {
-    "Container": {
-      "p": 3,
-      "maxWidth": 1024
+  styles: {
+    Container: {
+      p: 3,
+      maxWidth: 1024
     },
-    "root": {
-      "fontFamily": "body",
-      "lineHeight": "body",
-      "fontWeight": "body"
+    root: {
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body"
     },
-    "h1": {
-      "variant": "textStyles.display"
+    h1: {
+      variant: "textStyles.display"
     },
-    "h2": {
-      "variant": "textStyles.heading",
-      "fontSize": 5
+    h2: {
+      variant: "textStyles.heading",
+      fontSize: 5
     },
-    "h3": {
-      "variant": "textStyles.heading",
-      "fontSize": 4
+    h3: {
+      variant: "textStyles.heading",
+      fontSize: 4
     },
-    "h4": {
-      "variant": "textStyles.heading",
-      "fontSize": 3
+    h4: {
+      variant: "textStyles.heading",
+      fontSize: 3
     },
-    "h5": {
-      "variant": "textStyles.heading",
-      "fontSize": 2
+    h5: {
+      variant: "textStyles.heading",
+      fontSize: 2
     },
-    "h6": {
-      "variant": "textStyles.heading",
-      "fontSize": 1
+    h6: {
+      variant: "textStyles.heading",
+      fontSize: 1
     },
-    "a": {
-      "color": "primary",
+    a: {
+      color: "primary",
       "&:hover": {
-        "color": "secondary"
+        color: "secondary"
       }
     },
-    "pre": {
-      "variant": "prism",
-      "fontFamily": "monospace",
-      "fontSize": 1,
-      "p": 3,
-      "color": "text",
-      "bg": "muted",
-      "overflow": "auto",
-      "code": {
-        "color": "inherit"
+    pre: {
+      variant: "prism",
+      fontFamily: "monospace",
+      fontSize: 1,
+      p: 3,
+      color: "text",
+      bg: "muted",
+      overflow: "auto",
+      code: {
+        color: "inherit"
       }
     },
-    "code": {
-      "fontFamily": "monospace",
-      "color": "secondary",
-      "fontSize": 1
+    code: {
+      fontFamily: "monospace",
+      color: "secondary",
+      fontSize: 1
     },
-    "inlineCode": {
-      "fontFamily": "monospace",
-      "color": "secondary",
-      "bg": "muted"
+    inlineCode: {
+      fontFamily: "monospace",
+      color: "secondary",
+      bg: "muted"
     },
-    "th": {
-      "verticalAlign": "bottom",
-      "borderBottomWidth": "2px"
+    th: {
+      verticalAlign: "bottom",
+      borderBottomWidth: "2px"
     },
-    "td": {
-      "verticalAlign": "top",
-      "borderBottomWidth": "1px"
+    td: {
+      verticalAlign: "top",
+      borderBottomWidth: "1px"
     },
-    "hr": {
-      "border": 0,
-      "borderBottom": "1px solid",
-      "borderColor": "muted"
+    hr: {
+      border: 0,
+      borderBottom: "1px solid",
+      borderColor: "muted"
     },
-    "img": {
-      "maxWidth": "100%"
+    img: {
+      maxWidth: "100%"
     }
   },
   containers: {
