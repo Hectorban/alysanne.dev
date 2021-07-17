600f0c5925e1ec42e1778e74274b9cf405346a29
Hectorban <hectorban1234@gmail.com>
2021-07-17 05:43:39 -0500

    Added an image as anchor tag for github

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,10 @@
-/** @jsxRuntime classic /
-/* @jsx jsx */
-import { jsx } from 'theme-ui'
+/** @jsxImportSource theme-ui */
 import Link from 'next/link'
+import Image from 'next/image'
+import { FC } from 'react'
 
-const Nav = () => (
-  <header sx={{height: '60px', width: '100vw', bg: 'primary', borderBottom: '1px solid', borderColor: 'primary'}}>
+const Nav:FC = () => (
+  <header sx={{height: '60px', width: '100vw', bg: 'highlight', borderBottom: '1px solid', borderColor: 'highlight'}}>
     <nav sx={{display: 'flex', alignItems: 'center',  justifyContent: 'space-between', variant: 'containers.page', height: '100%'}}>
       <Link href="/">
         <a sx={{fontWeight: 'bold', fontSize: 4, cursor: 'pointer'}}>Note App</a>
@@ -13,16 +13,16 @@ const Nav = () => (
       <Link href="/notes">
         <a sx={{color: 'text', fontSize: 3, cursor: 'pointer'}}>notes</a>
       </Link>
-      <a sx={{
-          color: 'text',
-          fontSize: 3,
-          cursor: 'pointer'
-        }}
+      <a sx={{color: 'text',fontSize: 3,cursor: 'pointer'}}
         href='https://github.com/Hectorban'
       >
-        My Github
-</a>
-
+        <Image 
+          src='https://i.imgur.com/EWm6VSu.png'
+          alt='github'
+          width={32}
+          height={32}
+        />
+      </a>
     </nav>
   </header>
 )
