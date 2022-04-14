43a92c1bafd441e08bbf9f4ac938c9a4035a0352
Héctor Banderas <hectorban1234@gmail.com>
2022-04-14 03:37:08 -0500

    chore: deleted unnecesary files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 /** @jsxImportSource theme-ui */
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'theme-ui'
-import theme from '../src/theme/theme'
 import Nav from '../src/components/nav'
 
 function App({Component, pageProps}: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <Nav />
-        <Component {...pageProps} />
-      </div>      
-    </ThemeProvider>
+    <div>
+      <Nav />
+      <Component {...pageProps} />
+    </div>      
   )
 }
 
